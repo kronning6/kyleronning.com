d00be92acb45106586232161a22a7e3ad1fe97b2
Kyle Ronning <kyleronning+kronning6@gmail.com>
2026-04-17 19:35:00 -0500

    feat: migrate to next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-mono bg-white">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
+        Kyle Ronning
+      </main>
+    </div>
+  );
+}
